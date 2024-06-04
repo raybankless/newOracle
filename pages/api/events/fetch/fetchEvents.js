9f31398e7dfb979079a339d81e30de21cc38a4a7
raybankless <100173392+raybankless@users.noreply.github.com>
2024-06-04 18:35:49 +0000

    hooks

--- pages/api/events/fetch/fetchEvents.js
+++ pages/api/events/fetch/fetchEvents.js
@@ -3,70 +3,64 @@ import { HypercertClient } from "@hypercerts-org/sdk";
 import {
   createThirdwebClient,
   useAddress,
-  useSigner,
+  useSigner as thirdwebUseSigner,
 } from "@thirdweb-dev/react";
 import { viemAdapter } from "thirdweb/adapters/viem";
 import { useState } from 'react';
 
-
-function wallet () {
+// Custom hook for wallet
+function useWallet() {
   const [currentWallet, setCurrentWallet] = useState(useAddress());
   return currentWallet;
 }
 
-function signer () {
-  const [smartSigner, setSmartSigner] = useState(useSigner());
+// Custom hook for signer
+function useSignerHook() {
+  const [smartSigner, setSmartSigner] = useState(thirdwebUseSigner());
   return smartSigner;
 }
-
-
 
 export async function fetchEvents(setEventsCallback) {
   const twClient = createThirdwebClient({
     clientId: "22f2a1f2653b1f091455a59z951c2ecca",
   });
-//sign
+
+  //sign
   const viemClientWallet = viemAdapter.walletClient.toViem({
     client: twClient,
     chain: 10,
-    account: signer,
+    account: useSignerHook,
   });
   const client = new HypercertClient({
     chain: { id: 10 },
     walletClient: viemClientWallet,
-    easContractAddress: wallet,
+    easContractAddress: useWallet,
   });
 
   try {
     const ownedEvents = await client.indexer.claimsByOwner(account);
     const eventsWithMetadata = await Promise.all(
       ownedEvents.claims.map(async (event) => {
-        // Skip fetching metadata for events with invalid or empty URIs
         if (!event.uri || event.uri.startsWith("{") || event.uri === "") {
-          //console.log(`Skipping event due to invalid or empty URI: ${event.uri}`,);
           return null;
         }
-
         try {
           const metadata = await client.storage.getMetadata(event.uri);
-          // Check if the description starts with "GoodOracle Event"
           if (metadata.description.startsWith("GoodOracle Event:")) {
             return {
               ...event,
               metadata: metadata,
             };
           } else {
-            //  console.log(`Skipping event as it's not marked as a GoodOracle Event: ${metadata.description}`);
             return null;
           }
         } catch (error) {
           console.error(`Failed to fetch metadata for event:`, error);
           return null;
         }
-      }),
+      })
     );
 
-    // Filter out null values (events without valid metadata)
     const validEventsWithMetadata = eventsWithMetadata.filter(
       (event) => event !== null,
     );
