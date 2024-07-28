690d927ec7e055818338b5c041298580e6182714
raybankless <100173392+raybankless@users.noreply.github.com>
2024-07-28 14:18:36 +0000

    .

--- pages/allo.tsx
+++ pages/allo.tsx
@@ -1,12 +1,14 @@
 // pages/allo.tsx
+import React from "react";
 import Sidebar from "../components/Sidebar";
-import AlloContractInteraction from "../components/AlloInteraction";
+import AlloContractInteraction from "../components/AlloContractInteraction";
 
-const AlloPage = () => {
+const AlloPage: React.FC = () => {
+  console.log("Rendering AlloPage");
   return (
-    <div>
+    <div style={{ display: "flex" }}>
       <Sidebar />
-      <main>
+      <main style={{ marginLeft: "20px", flexGrow: 1 }}>
         <h1>Allo Interaction</h1>
         <AlloContractInteraction />
       </main>
